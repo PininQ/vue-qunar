1dce47b3cacfc71adc11d9a7a1189edb13645b9b
qinbin52qiul <im.qinbin@gmail.com>
2018-09-24 10:34:22 +0800

    热销推荐和周末去哪儿组件

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Weekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/ce/bc89bc2f0e33ea.jpg_r_640x214_3e408453.jpg',
+        title: '故宫',
+        desc: '东方宫殿建筑代表，世界宫殿建筑典范'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/ce/bc89bc2f0e33ea.jpg_r_640x214_3e408453.jpg',
+        title: '故宫',
+        desc: '东方宫殿建筑代表，世界宫殿建筑典范'
+      },
+      {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/ce/bc89bc2f0e33ea.jpg_r_640x214_3e408453.jpg',
+        title: '故宫',
+        desc: '东方宫殿建筑代表，世界宫殿建筑典范'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  margin-top 0.2rem
+  line-height 0.8rem
+  background #eee
+  text-indent 0.2rem
+.item-img-wrapper
+  overflow hidden
+  height 0
+  padding-bottom 33.4%
+  .item-img
+    width 100%
+.item-info
+  padding 0.1rem
+  .item-title
+    line-height 0.54rem
+    font-size 0.32rem
+    ellipsis()
+  .item-desc
+    line-height 0.4rem
+    color #ccc
+    ellipsis()
+</style>
