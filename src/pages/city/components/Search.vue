497ba2d83abd7822cf57c50615372e153e2d25e8
qinbin52qiul <im.qinbin@gmail.com>
2018-09-24 18:28:34 +0800

    城市选择项 - 搜索逻辑实现和性能优化

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,77 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom"
+            v-for="item of list"
+            :key="item.id"
+        >
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    // 节流函数：监听 keyword 改变
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      // 如果没有关键词，则把搜索结果置为空数组
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      // keyword 改变后，延迟 100ms 执行
+      this.timer = setTimeout(() => {
+        const result = []
+        // 对每一个 cities 进行遍历
+        for (let i in this.cities) {
+          // 对 cities 中的每一个键值对的值（spell和name）再进行一次遍历
+          this.cities[i].forEach(value => {
+            // 从 spell 和 name 中搜索关键词
+            if (value.spell.indexOf(this.keyword) > -1 ||
+                value.name.indexOf(this.keyword) > -1) {
+              // 把搜索到的关键词添加到 result 中
+              result.push(value)
+            }
+            // 将所有搜索到结果存到 list 中
+            this.list = result
+          })
+        }
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -25,5 +90,18 @@ export default {
     text-align center
     border-radius .3rem
     color #666
-
+.search-content
+  z-index 1
+  overflow hidden
+  position absolute
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  background #eee
+  .search-item
+    line-height .62rem
+    padding-left .2rem
+    background #fff
+    color #666
 </style>
